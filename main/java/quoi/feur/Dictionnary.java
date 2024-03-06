021cce2b1918bfc05bad57c67c53432d20dbfefb
vidalarnaud <barbinou40@gmail.com>
2024-03-06 13:44:06 +0100

    Exercice N°1.1 Paul arrive chez Matthieu.

--- main/java/quoi/feur/Dictionnary.java
+++ main/java/quoi/feur/Dictionnary.java
@@ -1,4 +1,12 @@
 package quoi.feur;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Dictionnary {
+    private final String name;
+    private final List<String> words = new ArrayList<>();
 }
--- test/java/quoi/feur/DictionnaryTest.java
+++ test/java/quoi/feur/DictionnaryTest.java
@@ -8,11 +8,21 @@ class DictionnaryTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        dictionnary = new Dictionnary();
+        dictionnary = new Dictionnary("sha256");
     }
 
     @Test
     void testInstantiate() {
         assertNotNull(dictionnary);
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("sha256", dictionnary.getName());
+    }
+
+    @Test
+    void testVerifyEmptyMot() {
+        assertTrue(dictionnary.getWords().isEmpty());
     }
 }
